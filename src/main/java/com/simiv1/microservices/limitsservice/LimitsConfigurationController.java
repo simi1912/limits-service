3ef97f1320faf331de5c8f70c7aec2779679f5a4
Simi <nioasca@gmail.com>
2021-04-16 14:57:10 +0300

    Added Hystrix fault tolerance.

--- src/main/java/com/simiv1/microservices/limitsservice/LimitsConfigurationController.java
+++ src/main/java/com/simiv1/microservices/limitsservice/LimitsConfigurationController.java
@@ -1,5 +1,6 @@
 package com.simiv1.microservices.limitsservice;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import com.simiv1.microservices.limitsservice.bean.LimitConfiguration;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +15,21 @@ public class LimitsConfigurationController {
     private Configuration configuration;
 
     @GetMapping("/limits")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieve")
     public LimitConfiguration retrieveLimitsFromConfigurations(){
 
         return  new LimitConfiguration(configuration.getMaximum(),
                 configuration.getMinimum());
+    }
+
+    @GetMapping("/fault-tolerance")
+    @HystrixCommand(fallbackMethod = "fallbackRetrieve")
+    public LimitConfiguration retrieveConfiguration(){
+        throw new RuntimeException("");
+    }
+
+    public LimitConfiguration fallbackRetrieve(){
+        return new LimitConfiguration(999,9);
     }
     
 }
